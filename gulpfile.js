bf7d796556705efb076b81710e51c13f6809cdfa
User <shaxulyan87@gmail.com>
2023-01-24 15:55:12 +0400

    Remove file

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,21 @@ const cleanCSS = require("gulp-clean-css");
 const connect = require("gulp-connect");
 
 gulp.task("styles", function () {
-  return (
-    gulp
-      .src("styles/*.scss")
-      .pipe(sass().on("error", sass.logError))
-      .pipe(concat("main.css"))
-      // .pipe(cleanCSS({ compatibility: "ie8" }))
-      .pipe(gulp.dest("styles"))
-      .pipe(connect.reload())
-  );
+  return gulp
+    .src("styles/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(concat("main.css"))
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(gulp.dest("styles"))
+    .pipe(connect.reload());
 });
 
 gulp.task("scripts", function () {
-  return (
-    gulp
-      .src("scripts/*.js")
-      .pipe(concat("main.js"))
-      // .pipe(uglify())
-      .pipe(gulp.dest("scripts"))
-  );
+  return gulp
+    .src("scripts/*.js")
+    .pipe(concat("main.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("scripts"));
 });
 
 gulp.task("connect", function () {
